d61be721f79c25c26f2e04c29be623b8c7cce425
green-redcloud3 <62781092+green-redcloud3@users.noreply.github.com>
2020-05-23 21:30:28 -0700

    this is a test

--- complete/src/main/java/com/example/securingweb/SecuringWebApplication.java
+++ complete/src/main/java/com/example/securingweb/SecuringWebApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SecuringWebApplication {
-
+	// this is a test
 	public static void main(String[] args) throws Throwable {
 		SpringApplication.run(SecuringWebApplication.class, args);
 	}
